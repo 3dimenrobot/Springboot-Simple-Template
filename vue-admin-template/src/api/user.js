e0130224a6805bbe2f3c5dd4de67f2d1c7bae423
dengyingxian <dengyingxian@cnbmtech.com>
2019-11-30 10:00:44 +0800

    前端使用模板。

---
+++ vue-admin-template/src/api/user.js
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+import Qs from 'qs'
+
+export function login(data) {
+  return request({
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    url: '/user/login',
+    method: 'post',
+    data: Qs.stringify(data)
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
